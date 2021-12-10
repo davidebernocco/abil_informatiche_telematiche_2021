5cf6e4151ac81c4c4e9e19e2054426b1c5a49192
DAVIDE BERNOCCO <95908565+davidebernocco@users.noreply.github.com>
2021-12-10 11:02:10 +0000

    Modifica

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def factorial(n):
-  if n == 0:
-    return 1
-  else:
-    return n * factorial(n - 1)
-print(factorial(4))
+import cmath
+import math
+a=math.sin(math.pi)
+b=math.cos(math.pi)
+z=complex(a,b)
+print("Il numero complesso è:", type(z), z)
+
+stringa1="Ma il cielo è sempre più blu!"
+print("Slicing della frase:", stringa1[4:8])
+print("Guarda che il meteo dà brutto...", stringa1)
